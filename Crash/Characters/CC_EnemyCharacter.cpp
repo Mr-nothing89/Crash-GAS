8910f5664a509f6d8c14ff8f756a36da54f4a5eb
Lucio Martinez <lu30ya31@hotmail.com>
2026-01-12 01:24:55 -0800

    Enemy Class

---
+++ Crash/Characters/CC_EnemyCharacter.cpp
@@ -0,0 +1,37 @@
+﻿// Mr. Nothing.
+
+
+#include "CC_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+// Sets default values
+ACC_EnemyCharacter::ACC_EnemyCharacter()
+{
+	
+	PrimaryActorTick.bCanEverTick = false;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+UAbilitySystemComponent* ACC_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+
+void ACC_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	if (!IsValid(AbilitySystemComponent)) return;
+	AbilitySystemComponent->InitAbilityActorInfo(this,this);
+	
+	if (!HasAuthority()) return;
+	
+	GiveStartupAbilities();
+}
+
+
---
+++ Crash/Characters/CC_EnemyCharacter.h
@@ -0,0 +1,27 @@
+﻿// Mr. Nothing.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CC_BaseCharacter.h"
+#include "CC_EnemyCharacter.generated.h"
+
+UCLASS()
+class CRASH_API ACC_EnemyCharacter : public ACC_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	
+	ACC_EnemyCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	
+	virtual void BeginPlay() override;
+	
+private:
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
