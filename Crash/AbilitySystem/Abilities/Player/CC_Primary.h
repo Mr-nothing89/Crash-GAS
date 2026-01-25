daa6b5f1599683b8568cac0a7f60d677cde2edee
Lucio Martinez <lu30ya31@hotmail.com>
2026-01-25 01:24:53 -0800

    Overlap Test

---
+++ Crash/AbilitySystem/Abilities/Player/CC_Primary.h
@@ -0,0 +1,32 @@
+﻿// Mr. Nothing.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Crash/AbilitySystem/Abilities/CC_GameplayAbility.h"
+#include "CC_Primary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRASH_API UCC_Primary : public UCC_GameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UFUNCTION(BlueprintCallable,Category="Crash|Abilities")
+	void HitBoxOverlapTest();
+	
+private:
+	
+	UPROPERTY(EditDefaultsOnly,Category="Crash|Abilities")
+	float HitBoxRadius = 100.f;
+	
+	UPROPERTY(EditDefaultsOnly,Category="Crash|Abilities")
+	float HitBoxForwardOffset = 200.f;
+	
+	UPROPERTY(EditDefaultsOnly,Category="Crash|Abilities")
+	float HitBoxElevationOffset = 20.f;
+};
