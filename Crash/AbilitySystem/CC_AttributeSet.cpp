7044b7a4bd8be700d92a869b27bd430d57e86253
Lucio Martinez <lu30ya31@hotmail.com>
2026-02-02 23:45:10 -0800

    AttributeSet

---
+++ Crash/AbilitySystem/CC_AttributeSet.cpp
@@ -0,0 +1,36 @@
+﻿// Mr. Nothing.
+
+
+#include "CC_AttributeSet.h"
+
+#include "Net/UnrealNetwork.h"
+
+void UCC_AttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME_CONDITION_NOTIFY(UCC_AttributeSet,Health,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCC_AttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCC_AttributeSet,Mana,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCC_AttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+}
+
+void UCC_AttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCC_AttributeSet,Health,OldHealth);
+}
+
+void UCC_AttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCC_AttributeSet,MaxHealth,OldMaxHealth);
+}
+
+void UCC_AttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCC_AttributeSet,Mana,OldMana);
+}
+
+void UCC_AttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCC_AttributeSet,MaxMana,OldMaxMana);
+}
---
+++ Crash/AbilitySystem/CC_AttributeSet.h
@@ -0,0 +1,53 @@
+﻿// Mr. Nothing.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSet.h"
+#include "CC_AttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS_BASIC(ClassName, PropertyName) \
+        GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+        GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+        GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+        GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/**
+ * 
+ */
+UCLASS()
+class CRASH_API UCC_AttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS_BASIC(UCC_AttributeSet,Health);
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS_BASIC(UCC_AttributeSet,MaxHealth);
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Mana)
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS_BASIC(UCC_AttributeSet,Mana);
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS_BASIC(UCC_AttributeSet,MaxMana);
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth);
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana);
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
+};
